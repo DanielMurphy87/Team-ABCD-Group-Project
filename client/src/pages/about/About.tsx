80433a02fae930f307b8fe93fbfd8eae9a7e4182
Bibilna Babu <bibilnababu@yahoo.com>
2023-03-24 11:08:47 +0000

    adding styles

--- client/src/pages/about/About.tsx
+++ client/src/pages/about/About.tsx
@@ -1,41 +1,47 @@
-import './about.scss'
+import './about.scss';
 
-const About:React.FC = () => 
+const About: React.FC = () => (
   <>
+    <div className="about">
+      <p className="about__paragraph">
+        {' '}
+        Are you ever unsure about what to prepare for your meals? It can be
+        difficult to decide on a homemade meal or find inspiration to utilize
+        the ingredients in your pantry. Additionally, mixing up a delicious
+        drink can be a fun challenge. Fortunately, the "What's for Dinner" app
+        is here to assist you in resolving these predicaments. It is your
+        ultimate solution to all these meal-planning and cooking dilemmas!
+      </p>
 
-   <div className='about'>
-
-     <p className='about__paragraph'> Are you ever unsure about what to prepare for your meals? It can be
-      difficult to decide on a homemade meal or find inspiration to utilize the
-      ingredients in your pantry. Additionally, mixing up a delicious drink can
-      be a fun challenge. Fortunately, the "What's for Dinner" app is here to
-      assist you in resolving these predicaments. It is your ultimate solution
-      to all these meal-planning and cooking dilemmas!</p>
-      
-      <p className='about__paragraph'> What's for Dinner is a user-friendly app designed to make meal planning
+      <p className="about__paragraph">
+        {' '}
+        What's for Dinner is a user-friendly app designed to make meal planning
         easy and stress-free. With our app, you can choose from a variety of
         recipes, We understand how challenging it can be to come up with new
         meal ideas, especially when you're busy. That's why we created What's
-        for Dinner - to make meal planning simple and enjoyable.</p>
-     
-     <p className='about__paragraph'>Our app was developed by the great developement team ABCD.</p>
-      
-     <p className='about__paragraph'> Our team is responsible for creating and maintaining the app, ensuring
-        that it is up-to-date and functioning properly. We work hard to ensure
-        that the app is user-friendly and intuitive, so you can easily navigate
-        it and find what you're looking for. We also add new recipes regularly,
-        so you never run out of meal ideas.</p>
-     
-     
-        <p className='about__paragraph'> In addition, we are always open to feedback and suggestions from our
+        for Dinner - to make meal planning simple and enjoyable.
+      </p>
+
+      <p className="about__paragraph">
+        Our app was developed by the great developement team <strong>ABCD</strong>.We are
+        responsible for creating and maintaining the app, ensuring that it is
+        up-to-date and functioning properly. We work hard to ensure that the app
+        is user-friendly and intuitive, so you can easily navigate it and find
+        what you're looking for. We also add new recipes regularly, so you never
+        run out of meal ideas.
+      </p>
+
+      <p className="about__paragraph">
+        {' '}
+        In addition, we are always open to feedback and suggestions from our
         users.And over the past two years we crossed 1 million users. If you
         have any ideas on how we can improve the app, please don't hesitate to
         let us know. We are committed to providing the best possible experience
         for our users. Thank you for choosing What's for Dinner! We hope that
-        our app makes meal planning easier and more enjoyable for you.</p>
-      
-        </div>
+        our app makes meal planning easier and more enjoyable for you.
+      </p>
+    </div>
   </>
-
+);
 
 export default About;
