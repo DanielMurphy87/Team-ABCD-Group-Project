b7c5b83796d7cfc215bc57de797286f28602ea00
cj-rupp <72820354+cj-rupp@users.noreply.github.com>
2023-03-25 21:07:20 +0000

    feat: Experimenting with font imports and routing.

--- client/src/navigation/RouterConfig.tsx
+++ client/src/navigation/RouterConfig.tsx
@@ -3,16 +3,19 @@ import Home from '../pages/home';
 import About from '../pages/about';
 import Layout from '../layout';
 import NotFound from '../pages/errors/NotFound';
-import { Suspense } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { SEARCH_ROUTE, BASE_ROUTE, ABOUT_ROUTE } from './pathConstant';
+import { Suspense, useState } from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { SEARCH_ROUTE, BASE_ROUTE, ABOUT_ROUTE, ERROR_ROUTE } from './pathConstant';
 import { ErrorBoundary } from 'react-error-boundary';
 import Error from '@/pages/errors/Error';
 
 const Search = React.lazy(() => import('pages/search'));
 
+/* <Navigate to={BASE_ROUTE+ERROR_ROUTE} state={setErrorMessage('Loading...')} /> */
 
 const RouterConfig: React.FC = () => {
+
+  const [errorMessage,setErrorMessage] = useState("Chill! No Errors are known to this System.");
 
   return (
     <ErrorBoundary fallback={<Error message="Something went Wrong!" />}>
@@ -23,6 +26,7 @@ const RouterConfig: React.FC = () => {
               <Route index element={<Home message="Steak" />} />
               <Route path={SEARCH_ROUTE} element={<Search />} />
               <Route path={ABOUT_ROUTE} element={<About />} />
+              <Route path={ERROR_ROUTE} element={<Error message={errorMessage} />} />
               <Route
                 path="*"
                 element={<Error message="404 File not found! (on this site)" />}
--- client/src/navigation/pathConstant.ts
+++ client/src/navigation/pathConstant.ts
@@ -2,3 +2,4 @@ export const BASE_ROUTE = '/';
 export const HOME_ROUTE = 'home';
 export const SEARCH_ROUTE = 'search';
 export const ABOUT_ROUTE = 'about';
+export const ERROR_ROUTE = 'errors';
