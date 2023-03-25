c0d70afedae6c60cee0f583e13e1bd04777e61e5
cj-rupp <72820354+cj-rupp@users.noreply.github.com>
2023-03-25 11:29:28 +0000

    fix: I corrected a conflict that I left in the React code in RouterConfig. I don't think that the About page needs a loading pause, just for loading an image.

--- client/src/navigation/RouterConfig.tsx
+++ client/src/navigation/RouterConfig.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Home from '../pages/home';/* 
-import Search from '../pages/search'; */
+import Home from '../pages/home';
+/* import Search from '../pages/search';  */
 import About from '../pages/about';
 import Layout from '../layout';
 import NotFound from '../pages/errors/NotFound';
@@ -9,7 +9,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HOME_ROUTE, SEARCH_ROUTE, BASE_ROUTE, ABOUT_ROUTE } from './pathConstant';
 
 const Search = React.lazy(() => import('pages/search'));
-const About=React.lazy(()=>import('pages/about'));
+/* const About=React.lazy(()=>import('pages/about')); */
 
 const RouterConfig: React.FC = () =>
 <Suspense fallback={<div>Loading...</div>}>  
