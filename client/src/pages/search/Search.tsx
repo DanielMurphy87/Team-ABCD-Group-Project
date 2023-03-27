4887f1e3a620ef146b4b5ac5510830e418e790ee
debugger <58630922+adevash07@users.noreply.github.com>
2023-03-27 08:21:16 +0100

    Build: Meal Api

--- client/src/pages/search/Search.tsx
+++ client/src/pages/search/Search.tsx
@@ -1,9 +1,43 @@
 import SearchFlexContainer, {
   SearchItem,
 } from './components/SearchFlexContainer';
+import { useLocation } from 'react-router-dom';
+import { useQuery } from 'react-query';
 import './search.scss';
+import { FOOD_API_BASE_URL, DRINKS_API_BASE_URL } from '@/config/config';
 
 const Search = () => {
+  const location = useLocation();
+  let searchTerm = location.pathname.split('/')[2].substring(1);
+  let searchType: string = location?.state?.searchType;
+
+  let base_url =
+    searchType === 'meals' ? FOOD_API_BASE_URL : DRINKS_API_BASE_URL;
+
+  console.log(searchType);
+  console.log(`${base_url}filter.php?i=${searchTerm}`);
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['search'],
+    queryFn: () => {
+      return fetch(`${base_url}filter.php?i=${searchTerm}`).then((res) =>
+        res.json()
+      );
+    },
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <>
+        <div>{`Error: ${error}`}</div>
+      </>
+    );
+  }
+
   return (
     <div className="searchPage container">
       <div className="searchPage__header">
@@ -19,8 +53,12 @@ const Search = () => {
       </div>
 
       <SearchFlexContainer>
-        {[...Array(10)].map((_, i) => (
-          <SearchItem key={i} />
+        {data.meals.map((item: any) => (
+          <SearchItem
+            idMeal={item.idMeal}
+            strMeal={item.strMeal}
+            strMealThumb={item.strMealThumb}
+          />
         ))}
       </SearchFlexContainer>
     </div>
