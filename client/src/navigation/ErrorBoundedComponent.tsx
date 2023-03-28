d7f8977034175d3fc48e8b53e15dcd5780422cc2
cj-rupp <72820354+cj-rupp@users.noreply.github.com>
2023-03-28 22:17:11 +0100

    chore: minor tidying

--- client/src/navigation/ErrorBoundedComponent.tsx
+++ client/src/navigation/ErrorBoundedComponent.tsx
@@ -8,47 +8,6 @@ interface ebcProps {
 const FallbackRender = ({ resetErrorBoundary, error }: FallbackProps) =>
   <ErrorComponent message={error?.message} />
 
-/*
-
-
-    <ScreenWrapper>
-      <ScrollView>
-        <MyHeader>App Crashed:</MyHeader>
-        <MyText style={{ marginVertical: 40, fontSize: 16 }}>
-          {error?.message}
-        </MyText>
-        <MyButton onPress={resetErrorBoundary}>reload app</MyButton>
-      </ScrollView>
-    </ScreenWrapper>
-  );
-
-const = ErrorFallback => ({error:Error resetErrorBoundary}) => {
-    return (
-      <div role="alert">
-        <p>Something went wrong:</p>
-        <pre>{error.message}</pre>
-        <button onClick={resetErrorBoundary}>Try again</button>
-      </div>
-    )
-  }
-  */
-  /*
-  const ui = (
-    <ErrorBoundary
-      fallbackRender={ErrorFallback}
-      onReset={() => {
-        // reset the state of your app so the error doesn't happen again
-      }}
-    >
-      <ComponentThatMayError />
-    </ErrorBoundary>
-  )
-
-interface ebcProps {
-    component: React.ReactNode;    
-}
-*/
-
 const ErrorBoundedComponent: React.FC<ebcProps> = ({component}) => {
 
     return (
